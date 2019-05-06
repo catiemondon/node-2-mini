d2bfe55d38b715237960f69f928f19689600b659
catiemondon <Cathmond25@gmail.com>
2019-05-06 12:30:40 -0600

    set up server and end points

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express= require('express')
+const bc= require('./controllers/books-controller')
+
+const app= express()
+app.use(express.json())
+app.use(express.static(__dirname + '/../build'))
+
+app.get('/api/books', bc.read)
+
+app.post('/api/books', bc.create)
+
+app.put('/api/books/:id', bc.update)
+
+app.delete('/api/books/:id', bc.delete)
+
+app.listen(4000, ()=>{
+    console.log('Listening on port 4000')
+})
